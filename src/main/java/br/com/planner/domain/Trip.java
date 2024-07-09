2ae9e15cf09d24403b4b0f121e5ebe62982047bb
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-09 19:34:57 -0300

    entidade Trip

---
+++ src/main/java/br/com/planner/domain/Trip.java
@@ -0,0 +1,38 @@
+package br.com.planner.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "trips")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "starts_at", nullable = false)
+    private LocalDateTime startsAt;
+
+    @Column(name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private boolean confirmed;
+
+    @Column(name = "owner_name", nullable = false)
+    private String ownerName;
+
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+}
