b360bf88537ad87dc0d7a9069a6816fbb0d90965
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-09 19:34:45 -0300

    estrutura criada pelo initializr

---
+++ src/main/java/br/com/planner/PlannerRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerRestApplication.class, args);
+	}
+
+}
