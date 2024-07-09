b0a7dac05106104812d22ffd62d24ca58a67c0d3
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-09 19:34:06 -0300

    estrutura criada pelo initializr

---
+++ src/test/java/br/com/planner/PlannerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
