28d8d59aba9bf6726307786f7f4002547dab789b
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-11 16:26:58 -0300

    metodo findByEmail:

--- src/main/java/br/com/planner/repositories/OwnerRepository.java
+++ src/main/java/br/com/planner/repositories/OwnerRepository.java
@@ -3,8 +3,10 @@ package br.com.planner.repositories;
 import br.com.planner.domain.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OwnerRepository extends JpaRepository<Owner, UUID> {
     boolean existsByEmail(String email);
+    Optional<Owner> findByEmail(String email);
 }
