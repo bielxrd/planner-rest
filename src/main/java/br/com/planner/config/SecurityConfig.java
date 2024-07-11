1d2eaf7e992b65341eced71510d905b42acad59f
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-11 01:41:02 -0300

    security config e bcrypt

---
+++ src/main/java/br/com/planner/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package br.com.planner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.authorizeHttpRequests(http -> {
+            http.requestMatchers("/owners/create").permitAll();
+            http.requestMatchers("/trips/**").permitAll();
+        });
+
+
+        return httpSecurity.build();
+    }
+
+}
