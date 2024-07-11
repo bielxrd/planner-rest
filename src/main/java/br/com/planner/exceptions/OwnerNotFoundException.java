0607806988f7eb45b4ed197893661923ae9affef
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-11 17:53:16 -0300

    exception para owner not found e token invalid

---
+++ src/main/java/br/com/planner/exceptions/OwnerNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.planner.exceptions;
+
+public class OwnerNotFoundException extends RuntimeException {
+
+        public OwnerNotFoundException(String message) {
+            super(message);
+        }
+}
---
+++ src/main/java/br/com/planner/exceptions/TokenInvalidException.java
@@ -0,0 +1,8 @@
+package br.com.planner.exceptions;
+
+public class TokenInvalidException extends RuntimeException {
+
+        public TokenInvalidException(String message) {
+            super(message);
+        }
+}
