a76867db2aa1218c6e77579eaacd19074bdb7502
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-11 01:39:35 -0300

    tabela owner

---
+++ src/main/java/br/com/planner/domain/Owner.java
@@ -0,0 +1,33 @@
+package br.com.planner.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity(name = "owners")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Trip> trips;
+
+}
