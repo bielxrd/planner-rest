a3f620dc9be175ed41387a88118f9e22843d8466
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-13 22:01:37 -0300

    libera aplicacao para qualquer local

--- src/main/java/br/com/planner/PlannerRestApplication.java
+++ src/main/java/br/com/planner/PlannerRestApplication.java
@@ -2,8 +2,10 @@ package br.com.planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class PlannerRestApplication {
 
 	public static void main(String[] args) {
