43d5ee3faee3194a16337d9e7acd4723b7be5e50
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-15 23:54:24 -0300

    fix

--- src/main/java/br/com/planner/domain/Trip.java
+++ src/main/java/br/com/planner/domain/Trip.java
@@ -2,6 +2,7 @@ package br.com.planner.domain;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Trip {
 
     @Id
