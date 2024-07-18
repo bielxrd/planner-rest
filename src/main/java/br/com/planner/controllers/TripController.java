7434bb2749f56571346458d73b09d5d3b16f8247
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-17 21:23:18 -0300

    feat: rotas para atualizar trip e confirmar trip

--- src/main/java/br/com/planner/controllers/TripController.java
+++ src/main/java/br/com/planner/controllers/TripController.java
@@ -1,13 +1,13 @@
 package br.com.planner.controllers;
 
-import br.com.planner.dto.trip.TripRequestDTO;
-import br.com.planner.dto.trip.TripCreateResponseDTO;
-import br.com.planner.dto.trip.TripResponseDTO;
+import br.com.planner.dto.trip.*;
 import br.com.planner.services.TripService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RestController
@@ -37,5 +37,19 @@ public class TripController {
     public ResponseEntity<TripResponseDTO> get(@PathVariable UUID tripId) {
         TripResponseDTO trip = this.tripService.getTripById(tripId);
         return ResponseEntity.ok(trip);
+    }
+
+    @PutMapping("/update/{tripId}")
+    public ResponseEntity<UpdateTripDTO> update(@PathVariable UUID tripId, @RequestBody UpdateTripDTO request) {
+        UpdateTripDTO updatedTrip = this.tripService.updateTrip(tripId, request);
+        return ResponseEntity.ok(updatedTrip);
+    }
+
+    @PatchMapping("/{tripId}/confirm")
+    public ResponseEntity<TripIdDto> confirm(@PathVariable UUID tripId, UriComponentsBuilder uriComponentsBuilder) {
+        TripIdDto tripIdDto = this.tripService.confirmTrip(tripId);
+        URI uri = uriComponentsBuilder.path("/trips/{tripId}").buildAndExpand(tripId).toUri();
+
+        return ResponseEntity.created(uri).body(tripIdDto);
     }
 }
