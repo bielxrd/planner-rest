dd7fbc0178a502519a77222de3f5de55098648d9
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-17 21:24:10 -0300

    arruma

--- src/main/java/br/com/planner/dto/trip/TripResponseDTO.java
+++ src/main/java/br/com/planner/dto/trip/TripResponseDTO.java
@@ -2,17 +2,20 @@ package br.com.planner.dto.trip;
 
 
 import br.com.planner.dto.participant.ParticipantResponseDTO;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 public class TripResponseDTO {
     private String destination;
     private LocalDateTime startsAt;
     private LocalDateTime endsAt;
     private String ownerName;
     private String ownerEmail;
+    private boolean confirmed;
     private List<ParticipantResponseDTO> participants;
 }
