8adfac42997161732b712c3f5669218104114f1d
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-08-07 02:56:13 -0300

    feat: exception para not found e already registred

---
+++ src/main/java/br/com/planner/exceptions/ActivityFoundException.java
@@ -0,0 +1,7 @@
+package br.com.planner.exceptions;
+
+public class ActivityFoundException extends RuntimeException {
+    public ActivityFoundException(String message) {
+        super(message);
+    }
+}
