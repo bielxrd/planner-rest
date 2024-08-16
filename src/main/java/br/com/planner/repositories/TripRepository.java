1a75aed330bf9cf682eaa16940e06a814d10ae85
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-08-15 22:07:45 -0300

    feat: metodo busca trips de um owner como participant

--- src/main/java/br/com/planner/repositories/TripRepository.java
+++ src/main/java/br/com/planner/repositories/TripRepository.java
@@ -4,6 +4,8 @@ import br.com.planner.domain.Trip;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +14,10 @@ import java.util.UUID;
 public interface TripRepository extends JpaRepository<Trip, UUID> {
     Page<Trip> findAllByOwnerId(UUID ownerId, Pageable pageable);
     Optional<Trip> findByDestinationContainingIgnoreCaseAndOwnerId(String filter, UUID ownerId);
+
+    @Query(value = "SELECT * FROM trips "+
+            "JOIN participants ON trips.id = participants.trip_id "+
+            "WHERE participants.owner_id = :ownerId",
+            nativeQuery = true)
+    Page<Trip> findTripsByParticipantOwnerId(@Param("ownerId") UUID ownerId, Pageable pageable);
 }
