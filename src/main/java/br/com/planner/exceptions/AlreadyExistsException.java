eb6cb6f0f457030df04df027695f2abb15bbadc6
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-08-21 01:58:27 -0300

    feat: exception generiac

---
+++ src/main/java/br/com/planner/exceptions/AlreadyExistsException.java
@@ -0,0 +1,9 @@
+package br.com.planner.exceptions;
+
+public class AlreadyExistsException extends RuntimeException {
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+
+
+}
