039229d9bf3a0fa5679491e40b38dd1ecbbdaa5b
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-08-23 15:30:56 -0300

    feat: sqs producer

---
+++ src/main/java/br/com/planner/services/sqs/SQSProducerService.java
@@ -0,0 +1,45 @@
+package br.com.planner.services.sqs;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.MessageAttributeValue;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class SQSProducerService {
+
+    private final AmazonSQSAsync sqs;
+
+    @Value("${QUEUE_URL}")
+    private String queueUrl;
+
+    public SendMessageResult sendEmailToQueue(String email, String type) {
+
+        Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
+        MessageAttributeValue messageAttributeValue = new MessageAttributeValue()
+                .withStringValue(type)
+                .withDataType("String");
+
+        messageAttributes.put("type", messageAttributeValue);
+
+        SendMessageRequest messageRequest = new SendMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withMessageBody(email)
+                .withMessageAttributes(messageAttributes);
+
+        SendMessageResult messageResult = sqs.sendMessage(messageRequest);
+
+        if (messageResult.getMessageId() != null) {
+            return messageResult;
+        }
+        return null;
+    }
+}
