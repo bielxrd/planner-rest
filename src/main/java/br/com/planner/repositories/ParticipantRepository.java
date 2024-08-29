ae4830aaf68b9cddac5d9d80e886f4b6afbf4c5b
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-08-29 04:30:34 -0300

    feat: query personalizada

--- src/main/java/br/com/planner/repositories/ParticipantRepository.java
+++ src/main/java/br/com/planner/repositories/ParticipantRepository.java
@@ -4,6 +4,8 @@ import br.com.planner.domain.Participant;
 import br.com.planner.domain.Trip;
 import br.com.planner.services.participant.ParticipantService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.swing.text.html.Option;
 import java.util.List;
@@ -14,6 +16,9 @@ public interface ParticipantRepository extends JpaRepository<Participant, UUID> 
     public List<Participant> findAllByTripId(UUID tripId);
     public Optional<Participant> findByTripId(UUID tripId);
     Optional<Participant> findByEmailAndTripId(String email, UUID tripId);
+    Participant findByEmail(String email);
 
+    @Query(value = "SELECT p.* FROM participants p WHERE p.email = :email", nativeQuery = true)
+    List<Participant> findByEmailQuery(@Param("email") String email);
 
 }
